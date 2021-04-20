f7b4cbe3772bfc0968d0184b37ebcc99530ba32c
unknown <mikus.meimanis.metta@inbox.lv>
2021-04-20 16:00:38 +0300

    Viss parejais

--- JS/global.js
+++ JS/global.js
@@ -1,1 +1,9 @@
-console.log('JavaScript')
+document.getElementById('pievienotDarbu').addEventListener('click', pogaNospiesta)
+
+function pogaNospiesta(){
+    console.log('Poga nospiesta!')
+
+    let popUp = document.getElementById('popUp');
+
+    popUp.style.display = 'block' ; 
+}
