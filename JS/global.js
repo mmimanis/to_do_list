d1194d57c31a0c01a093bccedd6606604b08feef
unknown <mikus.meimanis.metta@inbox.lv>
2021-04-27 13:35:57 +0300

    27.04

--- JS/global.js
+++ JS/global.js
@@ -1,9 +1,33 @@
-document.getElementById('jaunsDarbs').addEventListener('click', pogaNospiesta)
+const POP_UP = document.getElementById('popUp');
+let darbi = []
 
-function pogaNospiesta(){
-    console.log('Poga nospiesta!')
+document.getElementById('jaunsDarbs').addEventListener('click', () => {
+    POP_UP.style.display = 'block'
+})
 
-    let popUp = document.getElementById('popUp');
+document.getElementById('pievienotDarbu').addEventListener('click', () => {
+    POP_UP.style.display = 'none'
 
-    popUp.style.display = 'block' ; 
+    let darbs = {darbs: daramais.value, termins: termins.value};
+
+    daramais.value = "";
+    termins.value = "";
+
+    darbi.push(darbs)
+
+    render();
+})
+
+function render() {
+    let saraksts = document.getElementById('saraksts')
+    
+    for(let i = 0; i < darbi.length; i++) {
+        console.log(darbi[i])
+        let darbs = 
+        `<div class="darbs">
+            <h3>Darbs: ${darbi[i].daramais}</h3>
+            <h4>Termiņš: ${darbi[i].termins}</h4>
+        </div>`;
+        saraksts.innerHTML = darbs;
+    }
 }
